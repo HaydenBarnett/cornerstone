f8137b6d4b95df532fdd0eaf1924a6ec41a4f9ef
Hayden Barnett <hayden@Haydens-MacBook-Air.local>
2014-07-01 21:53:51 +1000

    Added basic footer

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
-		</div>
+
+            <footer>
+                <p>&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?></p>
+            </footer>
+
+        </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 		<script>(window.jQuery)||document.write('<script src="<?php bloginfo("template_directory"); ?>/assets/js/jquery.js"><\/script>')</script>
 		<script src="<?php bloginfo("template_directory"); ?>/assets/js/functions.js"></script>
 		<?php wp_footer(); ?>
-        
+
 	</body>
 </html>
