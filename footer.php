2af254c001d940e7f0775925d88f14aa6da00dfd
Hayden Barnett <hayden@Haydens-MacBook-Air.local>
2014-07-01 22:01:31 +1000

    Changed jquery version from 2.0.3 to 1.11.1

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 
         </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script>(window.jQuery)||document.write('<script src="<?php bloginfo("template_directory"); ?>/assets/js/jquery.js"><\/script>')</script>
 		<script src="<?php bloginfo("template_directory"); ?>/assets/js/functions.js"></script>
 		<?php wp_footer(); ?>
