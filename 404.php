05d34aa36c94170d1dbc9bf6c53efaf3ce522f86
Hayden Barnett <Giant2@192-168-1-17.tpgi.com.au>
2015-03-11 13:54:27 +1100

    Removed loop from 404 page

--- 404.php
+++ 404.php
@@ -1,22 +1,18 @@
 <?php get_header(); ?>
 
     <div id="content-inner" class="container">
+ 
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="entry-header">
+                <h1>Nothing found</h1>
+            </div>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="entry-content">
+                <p>Sorry it seems we cannot find what you were looking for.</p>
+            </div>
 
-                <div class="entry-header">
-                    <h1>Nothing found</h1>
-                </div>
-
-                <div class="entry-content">
-                    <p>Sorry it seems we cannot find what you were looking for.</p>
-                </div>
-
-            </article>
-
-        <?php endwhile; ?>
+        </article>
 
     </div>
 
