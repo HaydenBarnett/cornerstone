74a159d6054acb373468ead3f21f2ace83482ba2
HaydenBarnett <hayden.barnett@hotmail.com>
2015-05-06 15:58:59 +1000

    Improved css mixins

--- functions.php
+++ functions.php
@@ -27,23 +27,23 @@ if ( ! isset( $content_width ) ) {
 
 function primary_nav() {
     wp_nav_menu(
-    array(
-        'theme_location'  => 'primary',
-        'menu'            => '',
-        'container'       => 'div',
-        'container_class' => '',
-        'container_id'    => '',
-        'menu_class'      => 'menu',
-        'menu_id'         => '',
-        'echo'            => true,
-        'fallback_cb'     => 'wp_page_menu',
-        'before'          => '',
-        'after'           => '',
-        'link_before'     => '',
-        'link_after'      => '',
-        'items_wrap'      => '<ul>%3$s</ul>',
-        'depth'           => 0,
-        'walker'          => ''
+        array(
+            'theme_location'  => 'primary',
+            'menu'            => '',
+            'container'       => 'div',
+            'container_class' => '',
+            'container_id'    => '',
+            'menu_class'      => 'menu',
+            'menu_id'         => '',
+            'echo'            => true,
+            'fallback_cb'     => 'wp_page_menu',
+            'before'          => '',
+            'after'           => '',
+            'link_before'     => '',
+            'link_after'      => '',
+            'items_wrap'      => '<ul>%3$s</ul>',
+            'depth'           => 0,
+            'walker'          => ''
         )
     );
 }
