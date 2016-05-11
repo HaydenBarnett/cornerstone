6fe23b07103e30dc6591e1dee9c6a9dfa9d15805
Hayden Barnett <hayden.barnett@hotmail.com>
2016-05-11 12:35:13 +1000

    Custom post type example

--- functions.php
+++ functions.php
@@ -79,6 +79,39 @@ function primary_menu() {
    -------------------------------------------------------------- */
 
 
+// Create 'Project'
+// Custom post type & category example
+/*
+function create_project_post() {
+
+    register_post_type( 'project',
+        array(
+            'labels' => array(
+                'name' => __( 'Projects', 'cornerstone' ),
+                'singular_name' => __( 'Project', 'cornerstone' )
+            ),
+            'menu_icon' => 'dashicons-admin-multisite',
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'excerpt'),
+            'taxonomies' => array('project-category')
+        )
+    );
+
+    register_taxonomy(
+        'project-category',
+        'project',
+        array(
+            'label' => __( 'Project Category', 'cornerstone' ),
+            'hierarchical' => true,
+            'query_var' => true,
+            // 'rewrite' => array( 'slug' => 'project-category' )
+        )
+    );
+
+} add_action( 'init', 'create_project_post' );
+*/
+
 
 /* --------------------------------------------------------------
    Wordpress Setup
