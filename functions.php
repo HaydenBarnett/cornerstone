c2be05c4240e89425055ab5c137c884a3efff964
Hayden Barnett <hayden.barnett@hotmail.com>
2016-05-11 12:18:21 +1000

    Remove auto p tag around images

--- functions.php
+++ functions.php
@@ -85,6 +85,13 @@ function primary_menu() {
    -------------------------------------------------------------- */
 
 
+// Remove p tags from images
+
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+} add_filter('the_content', 'filter_ptags_on_images');
+
+
 // Set custom excerpt length
 
 function set_excerpt_length( $length ) {
