cd25fae68aa71d97c381bc1d7824e8a1b73ee9cc
Hayden Barnett <hello@haydenbarnett.com>
2018-08-13 19:35:30 +1000

    Moved html5shiv inclusion to functions.php

--- functions.php
+++ functions.php
@@ -285,6 +285,8 @@ function queue_scripts() {
     $theme_version = $theme['Version'];
     wp_enqueue_style('open-sans-font', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600', '');
     wp_enqueue_style('theme-styles', get_stylesheet_uri(), array(), $theme_version);
+    wp_enqueue_script('html5shiv', get_template_directory_uri() . '/js/html5shiv.js', array(), '3.7.3');
+    wp_script_add_data('html5shiv', 'conditional', 'lt IE 9');
     wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery.js', array(), '3.3.1', true);
     wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), $theme_version, true);
 } add_action( 'wp_enqueue_scripts', 'queue_scripts' );
--- header.php
+++ header.php
@@ -2,20 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="author" content="<?php bloginfo("name"); ?>">
-    <meta name="designer" content="<?php bloginfo("name"); ?>">
-    <meta name="rating" content="general">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <?php wp_head(); ?>
-
     <?php get_template_part( 'partials/analytics', 'header' ); ?>
-
-    <!--[if lt IE 9]>
-        <script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/html5shiv.js"></script>
-    <![endif]-->
-    
 </head>
 <body <?php body_class(); ?>>
 
