e2ab7bb7efe27e7fb9c390d363c5b28efe44c724
Hayden Barnett <hello@haydenbarnett.com>
2019-04-11 00:05:28 +1000

    Moved footer inline

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
     </main><!-- #main -->
     
-    <?php include locate_template('partials/structure/footer.php'); ?>
+    <footer id="footer" class="block block-lg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <?php include locate_template('partials/components/symbol.php'); ?>
+                </div>
+                <div class="col-sm-6">
+                    <?php include locate_template('partials/components/nav-footer.php'); ?>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <?php wp_footer(); ?>
 
