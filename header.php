a75cabe01ad05599ff3e975d110f4c57f368e830
Hayden Barnett <hello@haydenbarnett.com>
2019-04-11 00:05:10 +1000

    Moved header inline

--- header.php
+++ header.php
@@ -10,6 +10,17 @@
 
     <?php include locate_template('partials/analytics/analytics-body.php'); ?>
 
-    <?php include locate_template('partials/structure/header.php'); ?>
+    <header id="header" class="block block-border-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <?php include locate_template('partials/components/logo.php'); ?>
+                    <?php include locate_template('partials/components/nav-primary.php'); ?>
+                    <?php include locate_template('partials/components/nav-hamburger.php'); ?>
+                </div>
+            </div>
+        </div>
+        <?php include locate_template('partials/components/nav-mobile.php'); ?>
+    </header>
     
     <main id="main">
