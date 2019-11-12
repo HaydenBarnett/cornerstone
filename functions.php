eb6a2f76299fc6c5bc8bdc24ea98be276eb313f0
Hayden Barnett <hbarnett@tyro.com>
2019-11-12 16:56:39 +1100

    ACF blocks init placeholder

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ $directory = get_template_directory() . '/functions';
 
 include $directory . '/functions-common.php';
 include $directory . '/functions-plugins.php';
+include $directory . '/functions-acf-blocks.php';
 include $directory . '/functions-menus.php';
 include $directory . '/functions-widgets.php';
 include $directory . '/functions-posts.php';
